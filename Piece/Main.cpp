7399edba17a3a3158d7b7525471fa3e6e121fc26
ao1415 <taku31126816@yahoo.co.jp>
2016-10-03 00:00:02 +0900

    ポリゴンを利用した形状取得

--- Piece/Main.cpp
+++ Piece/Main.cpp
@@ -1,187 +1,81 @@
 ﻿#include <Siv3D.hpp>
-#include <stack>
 
-using namespace std;
-
-class ImageChip {
+class ExtraPiece {
 public:
 
-	static const Array<Image> chip(Image image) {
+	const Array<Point> getPiece(const Image& image, const double max_distance = 3.0) {
 
-		Grid<int> bmp(image.size, 0);
-		Grid<int> flag(image.size, -1);
+		const Polygon polygon = Imaging::FindContour(image, false);
+		const Array<Vec2> vecArray = polygon.simplified(max_distance).outer();
 
-		for (const auto& p : step(image.size))
-			bmp[p] = image[p].r;
-
-		const int pieceNumber = numbering(bmp, flag);
-
-		return chiper(bmp, flag);
-	}
-
-private:
-
-	static const inline bool inside(const Point& p, const Size& s) {
-		return (0 <= p.x && 0 <= p.y && p.x < s.x && p.y < s.y);
-	}
-
-	static const int numbering(const Grid<int>& bmp, Grid<int>& flag) {
-
-		const auto inside = [](const Point& p, const Size& s) {
-			return (0 <= p.x && 0 <= p.y && p.x < s.x && p.y < s.y);
-		};
-
-		int pieceCount = 0;
-		for (const auto& p : step(bmp.size()))
+		Array<Point> vec;
+		for (size_t i = 0, size = vecArray.size(); i < size; i++)
 		{
-			if (flag[p] != -1) continue;
+			const double range = Math::Sqrt(Math::Square(vecArray[i].x - vecArray[(i + 1) % size].x) + Math::Square(vecArray[i].y - vecArray[(i + 1) % size].y));
+			const double angel = Math::Atan2(vecArray[(i + 1) % size].y - vecArray[i].y, vecArray[(i + 1) % size].x - vecArray[i].x);
 
-			std::stack<Point> sta;
-
-			sta.push(p);
-			const int color = bmp[p];
-			flag[p] = pieceCount;
-
-			while (!sta.empty())
+			//最低の辺の長さ未満ならば辺を削除する
+			if (range > 15)
 			{
-				const auto point = sta.top();
-				sta.pop();
-
-				for (int dy = -1; dy <= 1; dy++)
-				{
-					for (int dx = -1; dx <= 1; dx++)
-					{
-						if (dy == 0 && dx == 0) continue;
-
-						const Point dp = point + Point(dx, dy);
-
-						if (inside(dp, bmp.size()) && flag[dp] == -1 && bmp[dp] == color)
-						{
-							sta.push(dp);
-							flag[dp] = pieceCount;
-						}
-
-					}
-				}
-
+				vec.push_back(Point(vecArray[i].x, vecArray[i].y));
 			}
-			pieceCount++;
-		}
-		/*
-		Image img(bmp.size(), Palette::Black);
-		for (const auto& p : step(bmp.size()))
-		{
-		const int c = 255 / pieceCount*flag[p];
-		img[p] = Color(c, c, c);
-		}
-		img.savePNG(L"切り分け.png");
-		*/
-		return pieceCount;
-	}
-
-	static const Array<Image> chiper(const Grid<int>& bmp, const Grid<int>& label) {
-
-		Array<Image> images;
-
-		int pieceCount = 1;
-		Grid<bool> flag(bmp.size(), false);
-
-		for (const auto& p : step(bmp.size()))
-		{
-			if (label[p] <= 0) continue;
-
-			if (label[p] == pieceCount && !flag[p])
+			else
 			{
-				Array<Point> points;
+				const Vec2 p1 = vecArray[(i + size - 1) % size];
+				const Vec2 p2 = vecArray[(i + 0) % size];
+				const Vec2 p3 = vecArray[(i + 1) % size];
+				const Vec2 p4 = vecArray[(i + 2) % size];
 
-				std::stack<Point> sta;
-				sta.push(p);
-				flag[p] = true;
-				points.push_back(p);
+				const Vec2 v1 = p2 - p1;
+				const Vec2 v2 = p3 - p4;
 
-				int left = p.x, right = p.x, up = p.y, down = p.y;
+				const double sinv1 = (v1.x != 0) ? (v1.y / v1.x) : (1);
+				const double sinv2 = (v2.x != 0) ? (v2.y / v2.x) : (1);
 
-				while (!sta.empty())
-				{
-					const Point point = sta.top();
-					sta.pop();
+				const double x1 = ((p1.x*sinv1 - p4.x*sinv2) - (p1.y - p4.y)) / (sinv1 - sinv2);
+				const double y1 = p1.y + sinv1*(x1 - p1.x);
+				const double x2 = ((p1.x*sinv1 - p4.x*sinv2) - (p1.y - p4.y)) / (sinv1 - sinv2);
+				const double y2 = p4.y + sinv2*(x2 - p4.x);
 
-					for (int dy = -1; dy <= 1; dy++)
-					{
-						for (int dx = -1; dx <= 1; dx++)
-						{
-							if (dy == 0 && dx == 0) continue;
-
-							const Point dp = point + Point(dx, dy);
-
-							if (inside(dp, bmp.size()) && !flag[dp] && label[dp] == pieceCount)
-							{
-								sta.push(dp);
-								points.push_back(dp);
-								flag[dp] = true;
-
-								left = std::min(left, dp.x);
-								right = std::max(right, dp.x);
-								up = std::min(up, dp.y);
-								down = std::max(down, dp.y);
-							}
-
-						}
-					}
-
-				}
-
-				Image chipImage({ right - left + 1,down - up + 1 }, Palette::Black);
-
-				for (const auto& point : points)
-				{
-					chipImage[point - Point(left, up)] = Palette::White;
-				}
-				images.push_back(chipImage);
-				pieceCount++;
+				vec.push_back(Point((int)x1, (int)y1));
 			}
-
 		}
 
-		return images;
+		return vec;
 	}
 
 };
 
-
 void Main()
 {
+	Image image(L"piece2.png");
+	Texture texture(image);
 
-	Webcam webcam;
+	Window::Resize(texture.size * 2);
 
-	if (!webcam.open(1, Size(1920, 1080)))
-		return;
-	if (!webcam.start())
-		return;
+	ExtraPiece extra;
 
-	Image original;
 	while (System::Update())
 	{
-		if (webcam.hasNewFrame())
+		texture.draw();
+
+		const auto vec = extra.getPiece(image);
+
+		for (size_t i = 0, size = vec.size(); i < size; i++)
 		{
-			webcam.getFrame(original);
-			break;
+			const Point p1 = Point(texture.size.x, 0) + vec[i];
+			const Point p2 = Point(texture.size.x, 0) + vec[(i + 1) % size];
+			Line(p1, p2).draw(Palette::Yellow);
 		}
+
+		texture.draw(Point(texture.size.x / 2, texture.size.y), Alpha(127));
+		for (size_t i = 0, size = vec.size(); i < size; i++)
+		{
+			const Point p1 = Point(texture.size.x / 2, texture.size.y) + vec[i];
+			const Point p2 = Point(texture.size.x / 2, texture.size.y) + vec[(i + 1) % size];
+			Line(p1, p2).draw(Palette::Yellow);
+		}
+
 	}
-
-	if (!FileSystem::Exists(L"./chip"))
-		FileSystem::CreateDirectories(L"./chip");
-
-	original.savePNG(L"./chip/カメラ.png");
-
-	Image extraction = original.grayscaled().thresholded(151);
-	const auto images = ImageChip::chip(extraction);
-
-	for (const auto& num : step(images.size()))
-		images[num].savePNG(L"./chip/" + Pad(num, { L'0',2 }) + L".png");
-
-	Console::Open();
-	cout << images.size() << endl;
-	system("pause");
 
 }
