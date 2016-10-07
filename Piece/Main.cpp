79429fd64bd8f5ff1b0a3db28c314f6779962e05
ao1415 <taku31126816@yahoo.co.jp>
2016-10-08 02:52:43 +0900

    辺の長さを伸ばす処理を追加中

--- Piece/Main.cpp
+++ Piece/Main.cpp
@@ -12,7 +12,7 @@ public:
 		const Polygon polygon = Imaging::FindContour(image, false);
 		const Array<Vec2> vecArray = polygon.simplified(max_distance).outer();
 
-		Array<Point> vec;
+		Array<Point> vec1;
 		for (size_t i = 0, size = vecArray.size(); i < size; i++)
 		{
 			const double range = Math::Sqrt(Math::Square(vecArray[i].x - vecArray[(i + 1) % size].x) + Math::Square(vecArray[i].y - vecArray[(i + 1) % size].y));
@@ -21,7 +21,7 @@ public:
 			//最低の辺の長さ未満ならば辺を削除する
 			if (range > 10)
 			{
-				vec.push_back(Point(vecArray[i].x, vecArray[i].y));
+				vec1.push_back(Point(vecArray[i].x, vecArray[i].y));
 			}
 			else
 			{
@@ -41,11 +41,28 @@ public:
 				const double x2 = ((p1.x*sinv1 - p4.x*sinv2) - (p1.y - p4.y)) / (sinv1 - sinv2);
 				const double y2 = p4.y + sinv2*(x2 - p4.x);
 
-				vec.push_back(Point((int)x1, (int)y1));
+				vec1.push_back(Point((int)x1, (int)y1));
 			}
 		}
 
-		return vec;
+		Array<Point> vec2;
+		for (size_t i = 0, size = vec1.size(); i < size; i++)
+		{
+			const Vec2 p1 = vec1[(i + 0) % size];
+			const Vec2 p2 = vec1[(i + 1) % size];
+			const Vec2 p3 = vec1[(i + 2) % size];
+
+			const double ang1 = Math::Atan2(p2.y - p1.y, p2.x - p1.x);
+			const double ang2 = Math::Atan2(p3.y - p2.y, p3.x - p2.x);
+
+			if (abs(ang1 - ang2) < Radians(1))
+			{
+				vec2.push_back(Point(p1.x, p1.y));
+				i++;
+			}
+		}
+
+		return vec1;
 	}
 
 };
@@ -329,48 +346,122 @@ struct Piece {
 	Array<Corner> corners;
 };
 
+struct PairMatch {
+	PairMatch() = default;
+	PairMatch(size_t _n1, size_t _n2, size_t _v1, size_t _v2, int s, double a, Point p, bool r) : n1(_n1), n2(_n2), v1(_v1), v2(_v2), score(s), angle(a), pos(p), reverce(r) {}
+
+	size_t n1;
+	size_t n2;
+	size_t v1;
+	size_t v2;
+	int score;
+
+	//もう一つのピース
+	double angle;
+	Point pos;
+	bool reverce;
+
+	const bool operator<(const PairMatch& other) const {
+		if (score < other.score) return true;
+		else if (score > other.score) return false;
+		if (n1 < other.n1) return false;
+		else if (n1 > other.n1) return true;
+		if (n2 < other.n2) return false;
+		else if (n2 > other.n2) return true;
+		return false;
+	}
+
+};
+
 class AI {
 public:
 
-	Array<std::pair<size_t, size_t>> think(const Array<Array<Point>>& points) {
+	Array<PairMatch> think(const Array<Array<Point>>& points) {
 
-		Array<std::pair<size_t, size_t>> pattern;
+		Array<PairMatch> pattern;
 
 		getData(points);
 
 		const size_t size = pieces.size();
-
-		struct PairMatch {
-			PairMatch() = default;
-			PairMatch(size_t _n1, size_t _n2, int s) : n1(_n1), n2(_n2), score(s) {}
-
-			size_t n1;
-			size_t n2;
-			int score;
-
-			const bool operator<(const PairMatch& other) const {
-				if (score < other.score) return true;
-				else if (score > other.score) return false;
-				if (n1 < other.n1) return false;
-				else if (n1 > other.n1) return true;
-				if (n2 < other.n2) return false;
-				else if (n2 > other.n2) return true;
-				return false;
-			}
-
-		};
 
 		priority_queue<PairMatch> scoreQue;
 		vector<PairMatch> scoreVec;
 
 		for (size_t i = 0; i < size; i++)
 		{
-			for (size_t j = i + 1; j < size; j++)
+			for (size_t j = 0; j < size; j++)
 			{
-				const int score = match(i, j);
+				if (i == j) continue;
+
+				pair<int, int> pair1;
+				pair<int, int> pair2;
+
+				const int score1 = match(i, j, false, pair1);
+				const int score2 = match(i, j, true, pair2);
+				const int score = max(score1, score2);
+
 				if (score > 0)
-					scoreVec.push_back(PairMatch{ i, j, score });
-				scoreQue.push(PairMatch{ i, j, score });
+				{
+					if (score1 > score2)
+					{
+						const size_t size1 = pieces[i].corners.size();
+						const size_t size2 = pieces[j].corners.size();
+
+						const Point pos1_1 = pieces[i].corners[(pair1.first + 0) % size1].point;
+						const Point pos1_2 = pieces[i].corners[(pair1.first + 1) % size1].point;
+						const Point pos2_1 = pieces[j].corners[(pair1.second + 0) % size2].point;
+						const Point pos2_2 = pieces[j].corners[(pair1.second + 1) % size2].point;
+
+						const double ang1 = Math::Atan2((double)pos1_2.y - pos1_1.y, (double)pos1_1.x - pos1_1.x);
+						const double ang2 = Math::Atan2((double)pos2_2.y - pos2_1.y, (double)pos2_1.x - pos2_1.x);
+
+						const double dif = ang1 - ang2;
+
+						const Point rot = Point(pos2_1.x*cos(dif) - pos2_1.y*sin(dif), pos2_1.x*sin(dif) + pos2_1.y*cos(dif));
+						const Point difP = pos1_1 - rot;
+
+						std::cout << "info" << std::endl;
+						std::cout << i << "," << j << std::endl;
+						std::cout << ang1 << "," << ang2 << std::endl;
+						std::cout << dif << std::endl;
+						std::cout << rot << std::endl;
+						std::cout << difP << std::endl;
+
+						scoreVec.push_back(PairMatch(i, j, pair1.first, pair1.second, score, dif, difP, false));
+						scoreQue.push(PairMatch(i, j, pair1.first, pair1.second, score, dif, difP, false));
+					}
+					else
+					{
+						const size_t size1 = pieces[i].corners.size();
+						const size_t size2 = pieces[j].corners.size();
+
+						const Point pos1_1 = pieces[i].corners[(pair1.first + 0) % size1].point;
+						const Point pos1_2 = pieces[i].corners[(pair1.first + 1) % size1].point;
+						const Point pos2_1 = pieces[j].corners[(pair1.second + 2) % size2].point;
+						const Point pos2_2 = pieces[j].corners[(pair1.second + 1) % size2].point;
+
+						const double ang1 = Math::Atan2((double)pos1_2.y - pos1_1.y, (double)pos1_1.x - pos1_1.x);
+						const double ang2 = Math::Atan2((double)pos2_2.y - pos2_1.y, (double)pos2_1.x - pos2_1.x);
+
+						const double dif = ang1 - ang2;
+
+						const Point rot = Point(pos2_1.x*cos(dif) - pos2_1.y*sin(dif), pos2_1.x*sin(dif) + pos2_1.y*cos(dif));
+						const Point difP = pos1_1 - rot;
+
+						std::cout << "info" << std::endl;
+						std::cout << i << "," << j << std::endl;
+						std::cout << ang1 << "," << ang2 << std::endl;
+						std::cout << dif << std::endl;
+						std::cout << rot << std::endl;
+						std::cout << difP << std::endl;
+
+						scoreVec.push_back(PairMatch(i, j, pair2.first, pair2.second, score, dif, difP, true));
+						scoreQue.push(PairMatch(i, j, pair2.first, pair2.second, score, dif, difP, true));
+					}
+
+				}
+
+
 			}
 		}
 
@@ -382,9 +473,9 @@ public:
 		while (!scoreQue.empty())
 		{
 			const auto data = scoreQue.top();
-			pattern.push_back({ data.n1,data.n2 });
 			scoreQue.pop();
 			printf("%3d, %3d: %d\n", (int)data.n1, (int)data.n2, data.score);
+			pattern.emplace_back(data);
 		}
 
 		return pattern;
@@ -432,10 +523,7 @@ private:
 
 	}
 
-	const int match(const size_t n1, const size_t n2) const {
-
-		const double angleErrer = 3.0 / 180 * Pi;
-		const double lengthErrer = 10;
+	const int match(const size_t n1, const size_t n2, bool reverce, pair<int, int>& maxRad) const {
 
 		const auto size1 = pieces[n1].corners.size();
 		const auto size2 = pieces[n2].corners.size();
@@ -446,16 +534,20 @@ private:
 		{
 			for (const auto j : step(size2))
 			{
-				int score = eval(n1, n2, i, j);
+				int score = eval(n1, n2, i, j, reverce);
 
-				maxScore = max(maxScore, score);
+				if (maxScore < score)
+				{
+					maxScore = score;
+					maxRad = { (int)i,(int)j };
+				}
 			}
 		}
 
 		return maxScore;
 	}
 
-	const int eval(const size_t pieceId1, const size_t pieceId2, const size_t cornerId1, const size_t cornerId2) const {
+	const int eval(const size_t pieceId1, const size_t pieceId2, const size_t cornerId1, const size_t cornerId2, bool reverce) const {
 
 		int score1 = 0;
 		int score2 = 0;
@@ -484,31 +576,36 @@ private:
 
 		bool flag;
 
-		flag = false;
-		if (abs(len1_1 - len2_1) < errerLenght) score1 += 1;
-		if (abs(len1_2 - len2_2) < errerLenght) { score1 += 3; flag = true; }
-		if (abs(len1_3 - len2_3) < errerLenght) score1 += 1;
+		if (!reverce)
+		{
+			flag = false;
+			if (abs(len1_1 - len2_1) < errerLenght) score1 += 1;
+			if (abs(len1_2 - len2_2) < errerLenght) { score1 += 3; flag = true; }
+			if (abs(len1_3 - len2_3) < errerLenght) score1 += 1;
 
-		if (abs(ang1_1 + ang2_1 - Pi) < errerAngle) score1 += 3;
-		if (abs(ang1_2 + ang2_2 - Pi) < errerAngle) score1 += (flag ? 5 : 3);
-		if (abs(ang1_3 + ang2_3 - Pi) < errerAngle) score1 += (flag ? 5 : 3);
+			if (abs(ang1_1 + ang2_1 - Pi) < errerAngle) score1 += 3;
+			if (abs(ang1_2 + ang2_2 - Pi) < errerAngle) score1 += (flag ? 5 : 3);
+			if (abs(ang1_3 + ang2_3 - Pi) < errerAngle) score1 += (flag ? 5 : 3);
 
-		if (abs(ang1_1 + ang2_1 - Pi * 2) < errerAngle) score1 += 3;
-		if (abs(ang1_2 + ang2_2 - Pi * 2) < errerAngle) score1 += (flag ? 5 : 3);
-		if (abs(ang1_3 + ang2_3 - Pi * 2) < errerAngle) score1 += (flag ? 5 : 3);
+			if (abs(ang1_1 + ang2_1 - Pi * 2) < errerAngle) score1 += 3;
+			if (abs(ang1_2 + ang2_2 - Pi * 2) < errerAngle) score1 += (flag ? 5 : 3);
+			if (abs(ang1_3 + ang2_3 - Pi * 2) < errerAngle) score1 += (flag ? 5 : 3);
+		}
+		else
+		{
+			flag = false;
+			if (abs(len1_1 - len2_3) < errerLenght) score2 += 1;
+			if (abs(len1_2 - len2_2) < errerLenght) { score2 += 3; flag = true; }
+			if (abs(len1_3 - len2_1) < errerLenght) score2 += 1;
 
-		flag = false;
-		if (abs(len1_1 - len2_3) < errerLenght) score2 += 1;
-		if (abs(len1_2 - len2_2) < errerLenght) { score2 += 3; flag = true; }
-		if (abs(len1_3 - len2_1) < errerLenght) score2 += 1;
+			if (abs(ang1_1 + ang2_3 - Pi) < errerAngle) score2 += 3;
+			if (abs(ang1_2 + ang2_2 - Pi) < errerAngle) score2 += (flag ? 5 : 3);
+			if (abs(ang1_3 + ang2_1 - Pi) < errerAngle) score2 += (flag ? 5 : 3);
 
-		if (abs(ang1_1 + ang2_3 - Pi) < errerAngle) score2 += 3;
-		if (abs(ang1_2 + ang2_2 - Pi) < errerAngle) score2 += (flag ? 5 : 3);
-		if (abs(ang1_3 + ang2_1 - Pi) < errerAngle) score2 += (flag ? 5 : 3);
-
-		if (abs(ang1_1 + ang2_3 - Pi * 2) < errerAngle) score2 += 3;
-		if (abs(ang1_2 + ang2_2 - Pi * 2) < errerAngle) score2 += (flag ? 5 : 3);
-		if (abs(ang1_3 + ang2_1 - Pi * 2) < errerAngle) score2 += (flag ? 5 : 3);
+			if (abs(ang1_1 + ang2_3 - Pi * 2) < errerAngle) score2 += 3;
+			if (abs(ang1_2 + ang2_2 - Pi * 2) < errerAngle) score2 += (flag ? 5 : 3);
+			if (abs(ang1_3 + ang2_1 - Pi * 2) < errerAngle) score2 += (flag ? 5 : 3);
+		}
 
 		return max(score1, score2);
 	}
@@ -570,8 +667,8 @@ void Main()
 	for (auto& img : images)
 	{
 		const auto vec = extra.getPiece(img);
-		//for (size_t i = 0, size = vec.size(); i < size; i++)
-		//	Line(vec[i], vec[(i + 1) % size]).overwrite(img, 4, Palette::Red);
+		for (size_t i = 0, size = vec.size(); i < size; i++)
+			Line(vec[i], vec[(i + 1) % size]).overwrite(img, 2, Palette::Red);
 
 		pieces.emplace_back(vec);
 	}
@@ -598,6 +695,7 @@ void Main()
 
 	for (const auto& i : step(images.size()))
 	{
+		images[i].forEach([](Color& c) {if (c.r < 127) c.a = 0; });
 		textures.push_back(Texture(images[i]));
 	}
 
@@ -605,30 +703,31 @@ void Main()
 
 	while (System::Update())
 	{
-		const int W = 6, H = 4;
+		const int W = 1, H = 1;
 		for (int y = 0; y < H; y++)
 		{
 			for (int x = 0; x < W; x++)
 			{
 				const auto size = Point(Window::Size().x / W, Window::Size().y / H) - Point(20, 20);
 				const Size hSize = { size.x / 2,size.y };
-
 				const auto pos = Point(x * Window::Size().x / W, y * Window::Size().y / H) + Point(10, 10);
+				const Point center = size / 2;
 
 				if ((y + scroll)*W + x < pattern.size())
 				{
-					const auto pieceId1 = pattern[(y + scroll)*W + x].first;
-					const auto pieceId2 = pattern[(y + scroll)*W + x].second;
+					const auto pieceId1 = pattern[(y + scroll)*W + x].n1;
+					const auto pieceId2 = pattern[(y + scroll)*W + x].n2;
 
-					double scal1 = min((double)hSize.x / images[pieceId1].size.x, (double)hSize.y / images[pieceId1].size.y);
+					textures[pieceId1].draw(pos + Point(0, 0));
+					textures[pieceId2].draw(pos + Point(center.x, 0));
 
-					double scal2 = min((double)hSize.x / images[pieceId2].size.x, (double)hSize.y / images[pieceId2].size.y);
+					Line(pieces[pieceId1][(pattern[(y + scroll)*W + x].v1 + 0) % pieces[pieceId1].size()] + pos + Point(0, 0),
+						pieces[pieceId1][(pattern[(y + scroll)*W + x].v1 + 1) % pieces[pieceId1].size()] + pos + Point(0, 0)).draw(2, Palette::Blue);
+					Line(pieces[pieceId2][(pattern[(y + scroll)*W + x].v2 + 0) % pieces[pieceId2].size()] + pos + Point(center.x, 0),
+						pieces[pieceId2][(pattern[(y + scroll)*W + x].v2 + 1) % pieces[pieceId2].size()] + pos + Point(center.x, 0)).draw(2, Palette::Blue);
+					//textures[pieceId2].rotate(pattern[(y + scroll)*W + x].angle).draw(pos + center - pattern[(y + scroll)*W + x].pos);
 
-					textures[pieceId1].scale(scal1).draw(pos);
-					textures[pieceId2].scale(scal2).draw(Point(pos.x + hSize.x, pos.y));
-
-					font(pieceId1).draw(pos, Palette::Gray);
-					font(pieceId2).draw(pos + Point(hSize.x, 0), Palette::Gray);
+					font(Format(pieceId1, L"-", pieceId2, L":", pattern[(y + scroll)*W + x].score)).draw(pos, Palette::Gray);
 
 					Rect(pos, size).drawFrame(1, 1, Palette::White);
 				}
